093bc95309a7ffdc09ea5957ba1264422a015efd
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-08-02 12:57:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(
+        (matchingDriver) => matchingDriver.toUpperCase() === name.toUpperCase()
+    )
+}
+
+function fuzzyMatch(drivers, firstLetter) {
+    return drivers.filter(
+        (letterFinder) => letterFinder.toUpperCase().indexOf(firstLetter.toUpperCase()) === 0
+    )
+}
+function matchName(driver, identicalName) {
+    return driver.filter((nameData) => nameData.name === identicalName)
+}
